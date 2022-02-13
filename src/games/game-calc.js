e7f5d4d8483208fe6d5af4156f5ec62fb5c499ac
Lyudmila Maksimova <idzenni@gmail.com>
2022-02-14 01:03:26 +0300

    add brain-calc

---
+++ src/games/game-calc.js
@@ -0,0 +1,34 @@
+import runGame, * as gamesRules from '../index.js';
+import randomNum from '../randomNum.js';
+
+const gameRules = 'What is the result of the expression?';
+const operators = ['+', '-', '*'];
+const randomOperator = operators[Math.floor(Math.random() * operators.length)];
+const num1 = randomNum();
+const num2 = randomNum();
+const operator = randomOperator;
+
+
+const rightAnswer = (num1, num2, operator) => {
+    switch (operator) {
+        case '+' :
+            return num1 + num2;  
+        case '-' : 
+            return num1 - num2;
+        case '*' :
+            return num1 * num2;
+    }
+};
+
+const calc = () => {
+    const num1 = randomNum();
+    const num2 = randomNum();
+    const operator = randomOperator;
+    const randomExpression = `${num1} ${operator} ${num2}`;
+    const correctAnswer = rightAnswer(num1, num2, operator);
+    return [randomExpression, String(correctAnswer)];
+};
+
+export default () => {
+    runGame(gameRules, calc);
+};
---
+++ src/index.js
@@ -0,0 +1,28 @@
+//import randomNum from '../../src/randomNum.js'; 
+import readlineSync from 'readline-sync';
+import { userName } from './cli.js';
+const roundCount = 3;
+let bye = `Congratulations, ${userName}!`;
+
+const  gameOver = (userAnswer, correctAnswer, userName) => {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+    bye = `Let's try again, ${userName}`;
+}
+
+const runGame = (gameRules, calc) => {
+  console.log(gameRules);
+  for (let i = 0; i < roundCount; i += 1) {
+    const [question, correctAnswer] = calc();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question(`Your answer: `);
+
+    if (userAnswer === correctAnswer) {
+        console.log('Correct!');
+    } else if (userAnswer !== correctAnswer) {
+        gameOver(userAnswer, correctAnswer, userName);
+        break;
+    }
+  }
+};
+
+export default runGame;
