f1587599069d155dd11aa0b99d0bad38de97320a
Lyudmila Maksimova <idzenni@gmail.com>
2022-02-21 00:49:18 +0300

    fix greetings

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 console.log('Welcome to the Brain Games!');
 export const userName = readlineSync.question('May I have your name? '); 
 const greetings = () => {
-    console.log(`Hello, ${userName}`);
+    console.log(`Hello, ${userName}!`);
 };
 
 export default greetings;
